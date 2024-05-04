7c0ce1284b70ad5f3cae6b923436e1fdd520067c
Aaron Zuspan <aa.zuspan@gmail.com>
2024-05-04 14:45:37 -0700

    Docstrings

--- src/sknnr_spatial/estimator.py
+++ src/sknnr_spatial/estimator.py
@@ -84,7 +84,24 @@ class ImageEstimator(AttrWrapper[EstimatorType]):
 
     @check_wrapper_implements
     def fit(self, X, y=None, **kwargs) -> ImageEstimator[EstimatorType]:
-        """Fit the wrapped estimator and return the wrapper."""
+        """
+        Fit an estimator from a training set (X, y).
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            The training input samples.
+
+        y : array-like of shape (n_samples,) or (n_samples, n_outputs)
+            The target values (class labels in classification, real numbers in
+            regression). Single-output targets of shape (n_samples, 1) will be squeezed
+            to shape (n_samples,) to allow consistent prediction across all estimators.
+
+        Returns
+        -------
+        self : ImageEstimator
+            The wrapper around the fitted estimator.
+        """
         # Squeeze extra y dimensions. This will convert from shape (n_samples, 1) which
         # causes inconsistent output shapes with different sklearn estimators, to
         # (n_samples,), which has a consistent output shape.
@@ -103,6 +120,29 @@ class ImageEstimator(AttrWrapper[EstimatorType]):
     def predict(
         self, X_image: ImageType, *, nodata_vals: NoDataType = None
     ) -> ImageType:
+        """
+        Predict target(s) for X_image.
+
+        Notes
+        -----
+        If X_image is not an image, the estimator's unmodified predict method will be
+        called instead.
+
+        Parameters
+        ----------
+        X_image : Numpy or Xarray image with 3 dimensions (y, x, band)
+            The input image. Features in the band dimension should correspond with the
+            features used to fit the estimator.
+        nodata_vals : float or sequence of floats, optional
+            NoData values to mask in the output image. A single value will be broadcast
+            to all bands while sequences of values will be assigned band-wise. If None,
+            values will be inferred if possible based on image metadata.
+
+        Returns
+        -------
+        y_image : Numpy or Xarray image with 3 dimensions (y, x, targets)
+            The predicted values.
+        """
         return predict(X_image, estimator=self, nodata_vals=nodata_vals)
 
     @check_wrapper_implements
@@ -110,6 +150,37 @@ class ImageEstimator(AttrWrapper[EstimatorType]):
     def kneighbors(
         self, X_image: ImageType, *, nodata_vals: NoDataType = None, **kneighbors_kwargs
     ) -> ImageType | tuple[ImageType, ImageType]:
+        """
+        Find the K-neighbors of each pixel in an image.
+
+        Returns indices of and distances to the neighbors for each pixel.
+
+        Notes
+        -----
+        If X_image is not an image, the estimator's unmodified kneighbors method will be
+        called instead.
+
+        Parameters
+        ----------
+        X_image : Numpy or Xarray image with 3 dimensions (y, x, band)
+            The input image. Features in the band dimension should correspond with the
+            features used to fit the estimator.
+        nodata_vals : float or sequence of floats, optional
+            NoData values to mask in the output image. A single value will be broadcast
+            to all bands while sequences of values will be assigned band-wise. If None,
+            values will be inferred if possible based on image metadata.
+        **kneighbors_kwargs
+            Additional arguments passed to the estimator's kneighbors method, e.g.
+            `return_distance`.
+
+        Returns
+        -------
+        neigh_dist : Numpy or Xarray image with 3 dimensions (y, x, neighbor)
+            Array representing the lengths to points, only present if
+            return_distance=True.
+        neigh_ind : Numpy or Xarray image with 3 dimensions (y, x, neighbor)
+            Indices of the nearest points in the population matrix.
+        """
         return kneighbors(
             X_image, estimator=self, nodata_vals=nodata_vals, **kneighbors_kwargs
         )
