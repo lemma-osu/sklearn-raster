ae8ab0a75e9671662cc78d4db9125c8971ecd576
Aaron Zuspan <50475791+aazuspan@users.noreply.github.com>
2024-12-10 13:09:57 -0800

    Fix masking NaN values in input image (#34)
    * Add failing test case for masking input NaNs
    
    * Fix NaN masking by filling on-the-fly instead of at init
    
    I also moved nan_fill from an init parameter to an apply parameter,
    since it's more relevant to the function being applied than an
    attribute of the image chunk.
    
    I compared performance of filling with np.where versus copying
    and filling in-place by indexing, and the former was marginally
    faster.
    
    * Replace sknnr-specific test with more generic test
    
    * Reorganize function

--- tests/test_estimator.py
+++ tests/test_estimator.py
@@ -122,6 +122,20 @@ def test_kneighbors_unsupervised(model_data: ModelData, k):
     assert_array_equal(nn.shape, (k, model_data.n_rows, model_data.n_cols))
 
 
+@parametrize_model_data()
+def test_kneighbors_with_custom_kwarg(model_data: ModelData):
+    """Test that kneighbors passes custom kwargs."""
+
+    class CustomEstimator(KNeighborsRegressor):
+        def kneighbors(self, X, custom_kwarg, **kwargs):
+            assert custom_kwarg is not None
+            return super().kneighbors(X, **kwargs)
+
+    X_image, X, y = model_data
+    estimator = wrap(CustomEstimator()).fit(X, y)
+    unwrap_image(estimator.kneighbors(X_image, return_distance=False, custom_kwarg=1))
+
+
 @parametrize_model_data(image_types=(xr.DataArray,))
 def test_predict_dataarray_with_custom_dim_name(model_data: ModelData):
     """Test that predict works if the band dimension is not named "variable"."""
