abb7ea88ab085cbd1ecd0a5958f0cef778667831
Aaron Zuspan <aa.zuspan@gmail.com>
2025-03-17 15:30:55 -0700

    WIP partial support for 1D arrays
    So far this is mostly just addressing dimensions more generally
    so they're not explicitly 2D, which seems to get us most of the
    way to 1D support. We were failing 1 test as we require 1D samples
    to store features as the first dimension, which is consistent with
    numpy images and xarray images and samples, while Pandas-like
    samples are stored with features as the last dimension. For now,
    that's fixed by simply transposing the inputs and outputs.

--- src/sklearn_raster/utils/image.py
+++ src/sklearn_raster/utils/image.py
@@ -1,40 +1,11 @@
 from functools import wraps
 from typing import Callable
 
-import numpy as np
-import xarray as xr
 from numpy.typing import NDArray
-from typing_extensions import Any, Concatenate
+from typing_extensions import Concatenate
 
-from ..types import RT, ImageType, MaybeTuple, P
-from .wrapper import GenericWrapper, map_function_over_tuples
-
-
-def is_image_type(X: Any) -> bool:
-    # Feature array images must have exactly 3 dimensions: (y, x, band) or (band, y, x)
-    if isinstance(X, (np.ndarray, xr.DataArray)):
-        return X.ndim == 3
-
-    # Feature Dataset images must have exactly 2 dimensions: (x, y)
-    if isinstance(X, xr.Dataset):
-        return len(X.dims) == 2
-
-    return False
-
-
-def image_or_fallback(
-    func: Callable[Concatenate[GenericWrapper, ImageType, P], RT],
-) -> Callable[Concatenate[GenericWrapper, ImageType, P], RT]:
-    """Decorator that calls the wrapped method for non-image X arrays."""
-
-    @wraps(func)
-    def wrapper(self: GenericWrapper, X_image: ImageType, *args, **kwargs):
-        if not is_image_type(X_image):
-            return getattr(self._wrapped, func.__name__)(X_image, *args, **kwargs)
-
-        return func(self, X_image, *args, **kwargs)
-
-    return wrapper
+from ..types import MaybeTuple, P
+from .wrapper import map_function_over_tuples
 
 
 def image_to_samples(
@@ -67,7 +38,7 @@ def image_to_samples(
 
         @map_function_over_tuples
         def unflatten(r: NDArray) -> NDArray:
-            return r.reshape(*image.shape[:2], -1)
+            return r.reshape(*image.shape[:-1], -1)
 
         return unflatten(result)
 
