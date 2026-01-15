8628dac547780e45402b2f5453d4a167ff998056
Aaron Zuspan <aa.zuspan@gmail.com>
2026-01-15 15:40:27 -0800

    Rename ufunc tests for clarity

--- tests/test_ufunc.py
+++ tests/test_ufunc.py
@@ -62,7 +62,7 @@ def test_ufunc_return_type_priority(
 
 @parametrize_feature_array_types()
 @pytest.mark.parametrize("skip_nodata", [True, False])
-def test_input_array_not_mutated(
+def test_ufunc_does_not_mutate_input(
     feature_array_type: type[FeatureArrayType], skip_nodata: bool
 ):
     """Ensure that applying a ufunc to features doesn't mutate the original array."""
@@ -87,7 +87,7 @@ def test_input_array_not_mutated(
 @pytest.mark.parametrize(
     "val_dtype", [(-1, np.dtype(np.uint8)), (np.nan, np.dtype(np.int16))]
 )
-def test_nodata_output_with_unsupported_dtype(
+def test_ufunc_raises_with_unsupported_nodata_output_dtype(
     val_dtype: tuple[int | float, np.dtype],
     feature_array_type: type[FeatureArrayType],
     skip_nodata: bool,
@@ -124,7 +124,7 @@ def test_nodata_output_with_unsupported_dtype(
 @pytest.mark.parametrize(
     "dtypes", [(np.float64, np.dtype(np.float32)), (np.float32, np.dtype(np.float16))]
 )
-def test_nodata_output_float_precision(
+def test_ufunc_raises_with_unsupported_nodata_output_float_precision(
     feature_array_type: type[FeatureArrayType], dtypes: tuple[np.dtype, np.dtype]
 ):
     """Test that an error is raised when nodata_output is the wrong float precision."""
@@ -156,7 +156,7 @@ def test_nodata_output_float_precision(
 @pytest.mark.parametrize(
     "val_dtypes", [(-1, np.uint8, np.int8), (np.nan, np.int16, np.float64)]
 )
-def test_nodata_output_with_allow_cast(
+def test_ufunc_allows_casting_of_unsupported_nodata_output(
     val_dtypes: tuple[int | float, np.dtype, np.dtype],
     feature_array_type: type[FeatureArrayType],
     skip_nodata: bool,
@@ -190,7 +190,7 @@ def test_nodata_output_with_allow_cast(
 @pytest.mark.parametrize("nodata_output", [np.nan, 42.0])
 @parametrize_feature_array_types()
 @pytest.mark.parametrize("skip_nodata", [True, False])
-def test_nodata_output_set(
+def test_ufunc_sets_nodata_output(
     nodata_output: int | float,
     feature_array_type: type[FeatureArrayType],
     skip_nodata: bool,
@@ -223,7 +223,7 @@ def test_nodata_output_set(
 
 @pytest.mark.parametrize("n_features", [1, 2])
 @parametrize_feature_array_types()
-def test_shape_when_ufunc_squeezes_dimension(
+def test_ufunc_shape_when_squeezing_dimension(
     n_features: int, feature_array_type: type[FeatureArrayType]
 ):
     """Test the output shape when a ufunc squeezes the feature dimension."""
@@ -255,7 +255,7 @@ def test_shape_when_ufunc_squeezes_dimension(
 
 @parametrize_feature_array_types()
 @pytest.mark.parametrize("skip_nodata", [True, False])
-def test_warn_when_ufunc_returns_nodata(
+def test_ufunc_warns_when_returning_nodata(
     feature_array_type: type[FeatureArrayType], skip_nodata: bool
 ):
     """Test that a warning is raised when `nodata_output` is returned by the ufunc."""
@@ -285,7 +285,7 @@ def test_warn_when_ufunc_returns_nodata(
 
 @pytest.mark.parametrize("min_samples", [0, 1, 30])
 @parametrize_feature_array_types()
-def test_ensure_min_samples(
+def test_ufunc_ensures_min_samples(
     min_samples: int, feature_array_type: type[FeatureArrayType]
 ):
     """Test that the correct number of minimum samples are passed."""
@@ -314,7 +314,7 @@ def test_ensure_min_samples(
 
 
 @parametrize_feature_array_types()
-def test_ensure_too_many_samples(feature_array_type: type[FeatureArrayType]):
+def test_ufunc_raises_on_ensure_too_many_samples(feature_array_type: type[FeatureArrayType]):
     """Test that an error is raised if ensure_min_samples is larger than the array."""
     a = np.full((1, 1, 10), np.nan, dtype=np.float64)
 
@@ -338,7 +338,7 @@ def test_ensure_too_many_samples(feature_array_type: type[FeatureArrayType]):
 
 
 @parametrize_feature_array_types()
-def test_ensure_min_samples_doesnt_overwrite(
+def test_ufunc_ensure_min_samples_does_not_overwrite(
     feature_array_type: type[FeatureArrayType],
 ):
     """
@@ -384,7 +384,7 @@ def test_ensure_min_samples_doesnt_overwrite(
 @pytest.mark.parametrize("num_valid", [0, 1, 3])
 @pytest.mark.parametrize("nodata_input", [-32768, np.nan])
 @parametrize_feature_array_types()
-def test_nodata_is_skipped(
+def test_ufunc_skips_nodata(
     num_valid: int,
     nodata_input: int | float,
     feature_array_type: type[FeatureArrayType],
@@ -419,7 +419,7 @@ def test_nodata_is_skipped(
 
 @parametrize_feature_array_types()
 @pytest.mark.parametrize("nan_fill", [None, 42.0])
-def test_nan_filled(feature_array_type: type[FeatureArrayType], nan_fill: float | None):
+def test_ufunc_fills_nans(feature_array_type: type[FeatureArrayType], nan_fill: float | None):
     """Test that NaNs in the features are filled before passing to func."""
     a = np.array([[[1, np.nan]]])
     features = FeatureArray.from_feature_array(
@@ -448,7 +448,7 @@ def test_nan_filled(feature_array_type: type[FeatureArrayType], nan_fill: float 
 
 
 @pytest.mark.parametrize("nodata_output", [np.nan, 0, -32768])
-def test_nodata_output_set_in_dataarray_attrs(nodata_output: int | float):
+def test_ufunc_sets_dataarray_fillvalue(nodata_output: int | float):
     """Test that the output NoData value is stored as the _FillValue for a DataArray."""
     a = np.array([[[1, 2, 3]]])
     features = FeatureArray.from_feature_array(
@@ -474,7 +474,7 @@ def test_nodata_output_set_in_dataarray_attrs(nodata_output: int | float):
 
 
 @pytest.mark.parametrize("nodata_output", [np.nan, 0, -32768])
-def test_nodata_output_set_in_dataset_attrs(nodata_output: int | float):
+def test_ufunc_sets_dataset_fillvalue(nodata_output: int | float):
     """Test that the output NoData value is stored as the _FillValue for a DataArray."""
     a = np.array([[[1, 2, 3]]])
     features = FeatureArray.from_feature_array(
@@ -501,7 +501,7 @@ def test_nodata_output_set_in_dataset_attrs(nodata_output: int | float):
 
 
 @parametrize_feature_array_types(feature_array_types=[xr.DataArray, xr.Dataset])
-def test_empty_output_sizes_error(feature_array_type: type[FeatureArrayType]):
+def test_ufunc_raises_on_missing_output_sizes(feature_array_type: type[FeatureArrayType]):
     """Test that missing output sizes raise a helpful error."""
     a = wrap_features(np.array([[[1, 2]]]), type=feature_array_type)
     features = FeatureArray.from_feature_array(a)
@@ -584,7 +584,7 @@ def test_ufunc_broadcasts_feature_dimensions():
 
 
 @pytest.mark.parametrize("thread_limit", [1, 2, 8])
-def test_inner_thread_limit(thread_limit: int):
+def test_ufunc_applied_with_inner_thread_limit(thread_limit: int):
     """
     Test that the inner thread limit is set within applied ufuncs.
 
